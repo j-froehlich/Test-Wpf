7be5c608b80ed3ddee54588fddd1356886d96968
j-froehlich <j-froehlich@users.noreply.github.com>
2021-06-08 18:23:23 +0200

    Add project files.

---
+++ Test-Wpf/buttonImage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Test_Wpf
+{
+    /// <summary>
+    /// Interaktionslogik für buttonImage.xaml
+    /// </summary>
+    public partial class buttonImage : UserControl
+    {
+        public buttonImage()
+        {
+            InitializeComponent();
+        }
+    }
+}
